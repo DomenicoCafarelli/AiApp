e13535fc12c1ec426564119722b91f80cc528aca
Domenico Cafarelli <94187765+DomenicoCafarelli@users.noreply.github.com>
2023-01-15 13:28:53 +0100

    created the Api request updated the UI Added the packages

---
+++ AiApp/AiApp/AiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiAppApp.swift
+//  AiApp
+//
+//  Created by Domenico Cafarelli on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
